1488a09e7920f88cb5953581a1ff668f5a48fa6c
pider485 <oleksijzolotarov4@gmail.com>
2024-03-02 20:33:59 +0200

    map move

--- game.py
+++ game.py
@@ -30,15 +30,17 @@ class Player(GameSprite):
             self.rect.x -= 3
         if pressed[K_d]:
             self.rect.x += 3
-class grass(GameSprite):
-    def __init__(self, x , y, ):
+class Grass(GameSprite):
+    def __init__(self, x , y,view):
         super().__init__('model\map\grass_1_new.png', x, y, 35, 35)
-
+        self.view = view
+        if view == "Winter":
+            self.image=transform.scale(image.load("model\map\cobble_blood_2_old.png"), (35,35))
 
 bg = transform.scale(image.load("model\map\grass_1_new.png"), (WIDTH, HEIGHT))
 player = Player('model\player\centaur_brown_female.png', 0 , 0, 30, 30)
 
-Grass = []
+grass = []
 
 
 with open('map\map_x0_y0.txt', 'r') as file:
@@ -47,7 +49,7 @@ with open('map\map_x0_y0.txt', 'r') as file:
     for line in map:
         for symbol in line:
             if symbol == 'G':
-                Grass.append(grass(x, y))
+                grass.append(Grass(x, y,""),)
             x += 35
         y += 35
         x = 0
@@ -61,7 +63,7 @@ while run:
         if e.type == QUIT:
             run = False
     window.blit(bg, (0, 0))
-    for g in Grass:
+    for g in grass:
         g.draw()
     player.update()
     player.draw()
